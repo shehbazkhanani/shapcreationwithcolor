7791cc71dd68722069e771aa34b66a64df0aea9d
shehbaz khanani <cuteboyusman49@gmail.com>
2022-09-30 12:00:46 +0500

    shape change with button

---
+++ app.js
@@ -0,0 +1,14 @@
+const circle = () => {
+  circleStyle = document.getElementById("circle");
+  circleStyle.className = "b";
+}
+
+const rectangle = () => {
+    rectangleStyle = document.getElementById("rectangle");
+    rectangleStyle.className = "c"
+}
+
+const square = () => {
+    squareStyle = document.getElementById("square");
+    squareStyle.className = "a";
+}
